074919649bab68631ffc6780b2737ffd2aa319cd
prynsh7 <priyanshraj45@gmail.com>
2023-05-05 16:46:47 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     email: "Supreeth.chintzz@gmail.com",
     availability: "Open for work",
     brand:
-      "I've always believed in delivering valuable experiences for my peers. Seeking an opportunity in the field of software engineering with a strong desire to design and develop incredible products.",
+      "This portfolio showcases my passion for designing and developing innovative software products. As a student, I have always been dedicated to delivering valuable experiences for my peers. With a strong knowledge of Python, C++, C, web3, and MERN stack, I am capable of implementing and launching new projects. I possess a quick learning ability for new software and technologies and can work collaboratively in a team by providing valuable support. Through this portfolio, you will witness my skills and dedication towards creating exceptional software solution",
   };
 
   return (
--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -10,17 +10,17 @@ const SocialIcons = () => {
 
   return (
     <div className="socialIcons" style={styles.socialIcons}>
-      <a className="icon" style={styles.icon} href="https://github.com/mdyeates">
-        <i className="fa-brands fa-github" aria-hidden="true" title="Michael Yeates' GitHub Profile"></i>
+      <a className="icon" style={styles.icon} href="https://github.com/Supreethsk05">
+        <i className="fa-brands fa-github" aria-hidden="true" title="Supreeths' GitHub Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/mdyeates/">
-        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Michael Yeates' LinkedIn Profile"></i>
+      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/supreeth-kadappanavar-672213224/">
+        <i className="fa-brands fa-linkedin" aria-hidden="true" title="Supreeths' LinkedIn Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.instagram.com/michael.yeates/">
-        <i className="fa-brands fa-instagram" aria-hidden="true" title="Michael Yeates' Instagram Profile"></i>
+      <a className="icon" style={styles.icon} href="https://www.instagram.com/supreeth_kadappanavar/">
+        <i className="fa-brands fa-instagram" aria-hidden="true" title="Supreeths' Instagram Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://twitter.com/mdyeates">
-        <i className="fa-brands fa-twitter" aria-hidden="true" title="Michael Yeates' Twitter Profile"></i>
+      <a className="icon" style={styles.icon} href="https://twitter.com/Supreeth_K">
+        <i className="fa-brands fa-twitter" aria-hidden="true" title="'Supreeth' Twitter Profile"></i>
       </a>
     </div>
   );
